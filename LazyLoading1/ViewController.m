d4dab6ba4e584a1a4742fae74bb0c488eb735ec9
Ashwani Choudhary <ashwanichoudhary25@gmail.com>
2015-09-04 02:37:06 +0530

    GCD LazyLoading
    Load table with the images from server

--- LazyLoading1/ViewController.m
+++ LazyLoading1/ViewController.m
@@ -9,15 +9,75 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSMutableArray *tableData;
+}
 @end
 
 @implementation ViewController
 
+#pragma mark - ViewLifeCycle
 - (void)viewDidLoad {
+    
+    tableData = [[NSMutableArray alloc]init];
+    
+    //Adding Static Url to the Array
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    [tableData addObject:@"https://upload.wikimedia.org/wikipedia/commons/e/ed/Alcedo_atthis2.jpg"];
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+
+#pragma mark - TableView_DataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+
+    //Got the ImageView From Cell ContentView
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:420];
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    
+    //Got the Indicator From Cell ContentView
+    UIActivityIndicatorView *spinner = (UIActivityIndicatorView *)[cell viewWithTag:421];
+    
+    
+    dispatch_queue_t concurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(concurrentQueue, ^{
+        //Download Image
+        NSData *image = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:[tableData objectAtIndex:indexPath.row]]];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            //Show Downloaded Image
+            imageView.image = [UIImage imageWithData:image];
+             [spinner removeFromSuperview];
+             [cell setNeedsLayout];
+ 
+        });
+    });
+  
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
